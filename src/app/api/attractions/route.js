6430bd1efed39cc60e5d581c4a1b6eba4e204bd4
AzureAD\ยุทธนาสีขวา <dellnm2546>
2024-07-24 20:22:37 +0700

    initial commit

---
+++ src/app/api/attractions/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { mysqlPool } from "@/utils/db";
+
+export async function GET(request) {
+    try {
+      const promisePool = mysqlPool.promise();
+      const [rows, fields] = await promisePool.query(`SELECT * FROM attractions;`);
+      return NextResponse.json(rows);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.error();
+    }
+  }
